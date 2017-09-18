ffa7d9a9e671a1508be1e389457587f3c0d9a132
Don Rauscher <donald.rauscher@gmail.com>
2017-09-18 04:25:53 +0000

    A GA plugin script to be added client-side

---
+++ ga_route.js
@@ -0,0 +1,17 @@
+function RouteGAData(tracker) {
+
+    ga(function(tracker) {
+        var originalSendHitTask = tracker.get('sendHitTask');
+        tracker.set('sendHitTask', function(model) {
+            var payLoad = model.get('hitPayload');
+            originalSendHitTask(model);
+            var routeRequest = new XMLHttpRequest();
+            routeRequest.setRequestHeader("Content-type", "application/json");
+            var routePath = "https://REGION-PROJECT.cloudfunctions.net/ingestGA";
+            routeRequest.open('GET', routePath + "?" + payLoad, true);
+            routeRequest.send();
+        });
+    });
+
+}
+ga('provide', 'ga_route_plugin', RouteGAData);
