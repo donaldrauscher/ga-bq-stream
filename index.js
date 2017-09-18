05afc77f47bc11f5044a6a88765323c565703e51
Don Rauscher <donald.rauscher@gmail.com>
2017-09-18 04:27:21 +0000

    Add real GA fields; POST-->GET

--- index.js
+++ index.js
@@ -2,19 +2,30 @@ const config = require('./config.json');
 const bigquery = require('@google-cloud/bigquery')();
 
 exports.logGA = function logGA (req, res) {
-  console.log(req.body);
-  return res.status(200).send('Success: ' + req.body);
+  console.log(req.query);
+  return res.status(200).send('Success: ' + req.query);
 };
 
 exports.ingestGA = function ingestGA (req, res) {
   var dataset = bigquery.dataset(config.DATASET);
   var table = dataset.table(config.TABLE);
+  var params = req.query;
+  var now = new Date();
 
   var row = {
     json: {
-      from: req.body.from,
-      to: req.body.to,
-      message: req.body.message
+      version: params.v,
+      tracking_id: params.tid,
+      document_location: params.dl,
+      hit_type: params.t,
+      user_id: params.uid,
+      client_id: params.cid,
+      user_langage: params.ul,
+      event_category: params.ec,
+      event_action: params.ea,
+      event_label: params.el,
+      event_value: params.ev,
+      timestamp: now.toJSON()
     }
   };
   var options = {
