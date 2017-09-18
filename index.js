4b3a12dcd57d39d556205e70107bbcecddb6cf2b
Don Rauscher <donald.rauscher@gmail.com>
2017-09-18 01:30:44 +0000

    Update so we handle insert errors with callback function

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 const config = require('./config.json');
 const bigquery = require('@google-cloud/bigquery')();
 
-function getTable () {
-  const dataset = bigquery.dataset(config.DATASET);
-
-  return dataset.get({ autoCreate: true })
-    .then(([dataset]) => dataset.table(config.TABLE).get({ autoCreate: true }));
-}
-
 exports.logGA = function logGA (req, res) {
   console.log(req.body);
   return res.status(200).send('Success: ' + req.body);
 };
 
-exports.ingestGA = function ingestGA (event) {
-  const event_data = event.data;
-  return Promise.resolve()
-    .then(() => {
-      return getTable();
-    })
-    .then(([table]) => {
-      console.log(`Inserting ${ Array.isArray(event_data) ? event_data.length : 1 } records into BigQuery`);
-      return table.insert(event_data, {raw: true})
-        .then(function(data) {
-          var apiResponse = data[0];
-        })
-        .catch(function(err){
-          console.error(err);
-        });
-    });
+exports.ingestGA = function ingestGA (req, res) {
+  var dataset = bigquery.dataset(config.DATASET);
+  var table = dataset.table(config.TABLE);
+
+  var row = {
+    json: {
+      from: req.body.from,
+      to: req.body.to,
+      message: req.body.message
+    }
+  };
+  var options = {
+    raw: true
+  };
+
+  function insertHandler(err, apiResponse){
+    if (err){
+      res.status(500).send(err);
+    }
+    else {
+      res.status(200).send(apiResponse);
+    }
+  }
+
+  table.insert(row, options, insertHandler);
 };
